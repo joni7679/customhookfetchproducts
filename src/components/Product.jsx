38ef248e9bd642197be32f164c24801a8584253b
joni <codingmylife0@gmail.com>
2025-09-10 00:31:39 +0530

    complete my task...

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
 import ShimmerEffect from './ShimmerEffect';
 import useFetch from '../hooks/useFetch';
 function Product() {
@@ -16,16 +15,16 @@ function Product() {
             <h1 className='text-center font-light text-4xl mt-5 capitalize text-blue-700'>Our Products</h1>
             <div className='container mx-auto px-6 py-5 lg-px-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
                 {data.map((val, index) => {
-                    
+                    const { images, title, price } = val
                     return (
                         <div key={index} className='mx-w-md w-full p-3 rounded-2xl shadow-md'>
                             <div className='w-full h-[20rem]'>
-                                <img src={val.images} alt={val.images} className='w-full h-full object-cover rounded-3xl' />
+                                <img src={images} alt={images} className='w-full h-full object-cover rounded-3xl' />
                             </div>
                             <div className='product-info'>
-                                <p className='text-center text-xl font-semibold mt-5'>{val.title}</p>
+                                <p className='text-center text-xl font-semibold mt-5'>{title}</p>
                                 <div className='flex items-center '>
-                                    <p className='font-semibold'>₹{val.price}</p>
+                                    <p className='font-semibold'>₹{price}</p>
                                 </div>
                             </div>
                         </div>
--- src/components/ShimmerEffect.jsx
+++ src/components/ShimmerEffect.jsx
@@ -5,7 +5,7 @@ const ShimmerEffect = () => {
     return (
         <>
             <div className='container mx-auto px-6 py-5 lg-px-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-                {arry.map((val, index) => {
+                {arry.map((_, index) => {
                     return (
                         <div key={index} className='mx-w-md w-full p-3 rounded-2xl shadow-md animate-pulse bg-gray-200'>
                             <div className='w-[20rem] h-[20rem] animate-pulse bg-gray-200'>
